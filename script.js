5e09a703ff538d8f6809a8f79a0199b71c4b5794
Christoffer Lundberg <44975536+Lundbergarn@users.noreply.github.com>
2019-07-01 17:47:41 +0200

    added new webpage

--- script.js
+++ script.js
@@ -1,92 +1,112 @@
-$(document).ready(function () {
-  // Add smooth scrolling to all links
-  let upp = document.getElementById('upp');
-  upp.addEventListener('click', function (event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-      // Store hash
-      let hash = this.hash;
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({ scrollTop: $(hash).offset().top }, 800,
-        function () {
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        });
-    } // End if
+// Cache selectors
+var lastId,
+  topMenu = $("#mainNav"),
+  topMenuHeight = topMenu.outerHeight() + 1,
+  // All list items
+  menuItems = topMenu.find("a"),
+  // Anchors corresponding to menu items
+  scrollItems = menuItems.map(function () {
+    var item = $($(this).attr("href"));
+    if (item.length) { return item; }
   });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function (e) {
+  var href = $(this).attr("href"),
+    offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight - 50;
+  $('html, body').stop().animate({
+    scrollTop: offsetTop
+  }, 850);
+  e.preventDefault();
 });
 
+// Bind to scroll
+$(window).scroll(function () {
+  // Get container scroll position
+  var fromTop = $(this).scrollTop() + topMenuHeight;
 
-// Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function (event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-      &&
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      let target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 700, function () {
-          // Callback after animation
-          // Must change focus!
-          let $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
+  // Get id of current scroll item
+  var cur = scrollItems.map(function () {
+    if ($(this).offset().top - 100 < fromTop)
+      return this;
+  });
+  // Get the id of the current element
+  cur = cur[cur.length - 1];
+  var id = cur && cur.length ? cur[0].id : "";
+
+  if (lastId !== id) {
+    lastId = id;
+    // Set/remove active class
+    menuItems
+      .parent().removeClass("active")
+      .end().filter("[href=#" + id + "]").parent().addClass("active");
+  }
+});
+
+const navBar = document.getElementById('Nav');
+
+changeNavBackground = () => {
+  if (navBar) {
+    let scrollValue = window.pageYOffset;
+    if (scrollValue > 0) {
+      navBar.style.backgroundColor = "rgb(34,34,34)";
+    } else {
+      navBar.style.backgroundColor = "transparent";
+    }
+  }
+};
+
+window.addEventListener('scroll', debounce(changeNavBackground));
+
+
+
+
+
+// Image in on scroll
+function debounce(func, wait = 10, immediate = true) {
+  var timeout;
+  return function () {
+    var context = this, args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide() {
+  sliderImages.forEach(sliderImage => {
+    // half way through the image
+    const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+    // bottom of the image
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+
+    if (isHalfShown && isNotScrolledPast) {
+      sliderImage.classList.add('active');
+    } else {
+      sliderImage.classList.remove('active');
     }
   });
-
-
-// Hide navbar on scroll if less then 739
-const menu = document.getElementById("menu");
-
-if ($(window).width() < 739) {
-  let prevScrollpos = window.pageYOffset;
-  window.onscroll = function () {
-    let currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-      menu.style.top = "0";
-    } else {
-      menu.style.top = `-${menu.offsetHeight}px`;
-      menu.style.transition = "all 0.3s ease-out";
-    }
-    prevScrollpos = currentScrollPos;
-  }
 }
 
 
-//Hide watermark
+$(document).ready(debounce(checkSlide));
+window.addEventListener('scroll', debounce(checkSlide));
 
-// function hideImage() {
-//   setTimeout(function () {
-//     var image = document.getElementsByTagName("img");
-//     image[image.length - 1].style.display = 'none';
-//   }, 500);
-// };
-// hideImage();
-
-
-
-
+document.getElementById('about_me').addEventListener('click', function (e) {
+  // Get the position of target <br />
+  const position = $("#about").offset().top - topMenuHeight - 99;
+  // use jquery magic to get there ;) <br />
+  $("HTML, BODY").animate({ scrollTop: position }, 1000);
+})
 
